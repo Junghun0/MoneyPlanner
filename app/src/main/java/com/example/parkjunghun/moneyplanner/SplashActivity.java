b775c88cca0ca287a5d592c7403695afc3762621
parkjunghun <go9018@naver.com>
2018-12-13 16:32:32 +0900

    add : splash activity

---
+++ app/src/main/java/com/example/parkjunghun/moneyplanner/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.parkjunghun.moneyplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
